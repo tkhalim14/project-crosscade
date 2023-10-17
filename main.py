da12433bf42b418a2fb453f20bc021aae2426c02
Vss4969 <vidyasagar.singadi@gmail.com>
2023-10-17 23:08:27 +0545

    user model

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
-
+from fastapi_sqlalchemy import DBSessionMiddleware, db
 from gpt_turbo_api import *
+
+from schema import User as SchemaUser
+from models import User as ModelUser
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 app = FastAPI()
 
@@ -9,6 +18,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.post('/user/', response_model=SchemaUser)
+async def create_user(user: SchemaUser):
+    db_user = ModelUser(username=user.username, name=user.name, email=user.email, password=user.password)
+    db.session.add(db_user)
+    db.session.commit()
+    return db_user
 
 if __name__ == "__main__":
     import uvicorn
---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base  = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+
+
